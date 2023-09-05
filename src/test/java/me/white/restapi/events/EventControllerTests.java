93483a0007b51e134d380307ef3b6d7d0fede97b
itmsg-minseok <ka0ka0ka.ms@gmail.com>
2023-09-05 11:15:51 +0900

    Add create events test

--- src/test/java/me/white/restapi/events/EventControllerTests.java
+++ src/test/java/me/white/restapi/events/EventControllerTests.java
@@ -1,10 +1,34 @@
 package me.white.restapi.events;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
 @ExtendWith(SpringExtension.class)
 public class EventControllerTests {
+
+    @Autowired
+    MockMvc mockMvc;
+    // mock 웹서버를 띄우지 않기 때문에 웹서버보다는 빠르지만 단위테스트보다 빠르지는 않음
+
+    // api.json api.xml
+    @Test
+    public void createEvent() throws Exception {
+        mockMvc.perform(post("/api/events/")
+                    .contentType(MediaType.APPLICATION_JSON_UTF8)
+                    .accept(MediaTypes.HAL_JSON)
+                    )
+                .andExpect(status().isCreated());
+        
+    }
+
 }
